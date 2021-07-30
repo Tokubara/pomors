bce0f18127caec44784d27a90975b88a434add6b
Tokubara <1270802817@qq.com>
2021-07-30 16:48:54 +0800

    (feat,refactor) 功能上的主要区别是, 限定了番茄总数, 而不是无限循环. 见siyuan://blocks/20210730163232-45c1g3r

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::time::Duration;
 use exitfailure::ExitFailure;
 use structopt::StructOpt;
 use termion::raw::{IntoRawMode, RawTerminal};
+use std::thread::sleep;
 
 mod key_handler;
 mod notification;
@@ -15,12 +16,16 @@ mod view;
 
 #[derive(StructOpt)]
 struct Option {
-    #[structopt(short = "w", long = "work-sec", default_value = "1500")]
-    work_sec: u16,
-    #[structopt(short = "s", long = "short-break-sec", default_value = "300")]
-    short_break_sec: u16,
-    #[structopt(short = "l", long = "long-break-sec", default_value = "1200")]
-    long_break_sec: u16,
+    #[structopt(short = "w", long = "work", default_value = "25")]
+    work_min: u16,
+    #[structopt(short = "s", long = "short", default_value = "4")]
+    short_break_min: u16,
+    #[structopt(short = "l", long = "long", default_value = "8")]
+    long_break_min: u16,
+    #[structopt(short = "p", long = "per_long", default_value = "3")]
+    per_long: u16,
+    #[structopt(short = "n", long = "num", default_value = "8")]
+    num_pomo: u16,
 }
 
 fn main() -> Result<(), ExitFailure> {
@@ -32,63 +37,71 @@ fn main() -> Result<(), ExitFailure> {
 
     // start timer
     let mut stdout = stdout().into_raw_mode().unwrap();
-    let mut round: u64 = 1;
-    loop {
+    for round in 1..=args.num_pomo {
         // work timer
-        if start_timer(
-            args.work_sec,
-            round,
+        if start_timer( // 时间结束就返回, 时间结束是返回false, 未结束按退出, 是返回true
+            args.work_min*60,
+            round, // 这里传的是当前round, 传这个参数的作用并不是控制功能, 而是影响显示
+            args.num_pomo,
             &receiver,
             &mut stdout,
-            view::flush_work_timer,
+            view::flush_timer,
+            true
         )? {
             return Ok(());
         }
-
+        if round == args.num_pomo {
+            notification::send("All done!! \u{1F389}")?;
+            break;
+        }
         notification::send("it's time to take a break \u{2615}")?;
-        sound::play(sound::SoundFile::BELL)?;
+        // sound::play(sound::SoundFile::BELL)?;
 
         // break interval
-        view::flush_break_interval(&mut stdout)?;
-        if handle_input_on_interval(&mut stdout, &receiver)? {
-            return Ok(());
-        }
+        // view::flush_break_interval(&mut stdout)?; // 函数名中含有flush的全都是显示, 这个显示不包含时间信息
+        // if handle_input_on_interval(&mut stdout, &receiver)? { // 之前可以退出, 现在只有一个功能, 继续
+        //     return Ok(());
+        // }
 
         // break timer
-        let break_sec = if round % 4 == 0 {
-            args.long_break_sec
+        let break_sec = if round % args.per_long == 0 {
+            args.long_break_min * 60
         } else {
-            args.short_break_sec
+            args.short_break_min * 60
         };
         if start_timer(
             break_sec,
             round,
+            args.num_pomo,
             &receiver,
             &mut stdout,
-            view::flush_break_timer,
+            view::flush_timer,
+            false
         )? {
             return Ok(());
         }
 
         notification::send("it's time to work again!! \u{1F4AA}")?;
-        sound::play(sound::SoundFile::BELL)?;
+        // sound::play(sound::SoundFile::BELL)?;
 
         // work interval
-        view::flush_work_interval(&mut stdout)?;
-        if handle_input_on_interval(&mut stdout, &receiver)? {
-            return Ok(());
-        }
-
-        round += 1;
+        // view::flush_work_interval(&mut stdout)?;
+        // if handle_input_on_interval(&mut stdout, &receiver)? {
+        //     return Ok(());
+        // }
     }
+    Ok(())
 }
 
+// 返回有2种情况, remaining_sec为0, 或者按了退出, 但是前者返回的是Ok(false), 后者返回Ok(true)
 fn start_timer(
     remaining_sec: u16,
-    current_round: u64,
+    current_round: u16,
+    num_pomo: u16,
     receiver: &Receiver<key_handler::KeyAction>,
     stdout: &mut RawTerminal<Stdout>,
-    flush_fn: fn(s: &mut RawTerminal<Stdout>, t: u16, c: u64) -> Result<(), failure::Error>,
+    flush_fn: fn(s: &mut RawTerminal<Stdout>, t: u16, c: u16, num_pomo: u16, is_work: bool) -> Result<(), failure::Error>,
+    is_work: bool
 ) -> Result<bool, failure::Error> {
     let mut quited = false;
     let mut paused = false;
@@ -104,10 +117,10 @@ fn start_timer(
             _ => (),
         }
         if !paused {
-            flush_fn(stdout, remaining_sec, current_round)?;
-            remaining_sec -= 1;
+            flush_fn(stdout, remaining_sec, current_round, num_pomo, is_work)?;
+            remaining_sec -= 10;
         }
-        spin_sleep::sleep(Duration::from_secs(1));
+        sleep(Duration::from_secs(10));
     }
     Ok(quited)
 }
@@ -120,6 +133,7 @@ fn handle_input_on_timer(receiver: &Receiver<key_handler::KeyAction>) -> key_han
     }
 }
 
+// 根据键盘返回, 搞不懂, 如果已经有了handle_input_on_timer, 还要这个干啥
 fn handle_input_on_interval(
     stdout: &mut RawTerminal<Stdout>,
     receiver: &Receiver<key_handler::KeyAction>,
@@ -128,11 +142,11 @@ fn handle_input_on_interval(
     for received in receiver.iter() {
         match received {
             key_handler::KeyAction::Ok => break,
-            key_handler::KeyAction::Quit => {
-                view::release_raw_mode(stdout)?;
-                quited = true;
-                break;
-            }
+            // key_handler::KeyAction::Quit => {
+            //     view::release_raw_mode(stdout)?;
+            //     quited = true;
+            //     break;
+            // }
             _ => (),
         }
     }
--- src/view.rs
+++ src/view.rs
@@ -3,42 +3,44 @@ use std::io::Write;
 use failure::ResultExt;
 use termion::{clear, color};
 
-pub fn flush_work_timer(stdout: &mut impl Write, remaining_sec: u16, current_round: u64)
-                        -> Result<(), failure::Error> {
+pub fn flush_timer(stdout: &mut impl Write, remaining_sec: u16, current_round: u16, num_pomo: u16, is_work: bool) -> Result<(), failure::Error> {
     write!(
         stdout,
-        "{timer_cursor}{color}{clear}\u{1F345} {timer} (Round {current_round}){desc_cursor}[Q]: quit, [Space]: pause/resume",
+        "{timer_cursor}{color}{clear}{unicode} {timer} (Round {current_round}/{num_pomo}){color_reset}{desc_cursor}",
         timer_cursor = termion::cursor::Goto(2, 1),
-        color = color::Fg(color::Red),
+        color = if is_work {format!("{}", color::Fg(color::Red))} else {format!("{}", color::Fg(color::Green))},
+        unicode = if is_work {"\u{1F345}"} else {"\u{1F389}"}, 
         clear = clear::All,
         timer = convert_to_min(remaining_sec),
         current_round = current_round,
+        num_pomo = num_pomo,
+        color_reset = color::Fg(color::Reset),
         desc_cursor = termion::cursor::Goto(2, 2)
     ).context("failed to show work timer")?;
     stdout.flush().context("failed to flush work timer")?;
     Ok(())
 }
 
-pub fn flush_break_timer(stdout: &mut impl Write, remaining_sec: u16, current_round: u64)
-                         -> Result<(), failure::Error> {
-    write!(
-        stdout,
-        "{timer_cursor}{color}{clear}\u{2615} {timer} (Round {current_round}){desc_cursor}[Q]: quit, [Space]: pause/resume",
-        timer_cursor = termion::cursor::Goto(2, 1),
-        color = color::Fg(color::Green),
-        clear = clear::All,
-        timer = convert_to_min(remaining_sec),
-        current_round = current_round,
-        desc_cursor = termion::cursor::Goto(2, 2)
-    ).context("failed to show break timer")?;
-    stdout.flush().context("failed to flush break timer")?;
-    Ok(())
-}
+// pub fn flush_break_timer(stdout: &mut impl Write, remaining_sec: u16, current_round: u16)
+//                          -> Result<(), failure::Error> {
+//     write!(
+//         stdout,
+//         "{timer_cursor}{color}{clear}\u{2615} {timer} (Round {current_round}){desc_cursor} [Space]: pause/resume",
+//         timer_cursor = termion::cursor::Goto(2, 1),
+//         color = color::Fg(color::Green),
+//         clear = clear::All,
+//         timer = convert_to_min(remaining_sec),
+//         current_round = current_round,
+//         desc_cursor = termion::cursor::Goto(2, 2)
+//     ).context("failed to show break timer")?;
+//     stdout.flush().context("failed to flush break timer")?;
+//     Ok(())
+// }
 
 pub fn flush_break_interval(stdout: &mut impl Write) -> Result<(), failure::Error> {
     write!(
         stdout,
-        "{msg_cursor}{color}{clear}\u{1F389} press Enter to take a break{desc_cursor}[Q]: quit, [Enter]: start",
+        "{msg_cursor}{color}{clear}\u{1F389} press Enter to take a break{desc_cursor} [Enter]: start",
         msg_cursor = termion::cursor::Goto(2, 1),
         color = color::Fg(color::Green),
         clear = clear::All,
@@ -48,10 +50,11 @@ pub fn flush_break_interval(stdout: &mut impl Write) -> Result<(), failure::Erro
     Ok(())
 }
 
+// 这是休息结束, 打印work信息
 pub fn flush_work_interval(stdout: &mut impl Write) -> Result<(), failure::Error> {
     write!(
         stdout,
-        "{msg_cursor}{color}{clear}\u{1F514} press Enter to work!!{desc_cursor}[Q]: quit, [Enter]: start",
+        "{msg_cursor}{color}{clear}\u{1F514} press Enter to work!!{desc_cursor} [Enter]: start",
         msg_cursor = termion::cursor::Goto(2, 1),
         color = color::Fg(color::Red),
         clear = clear::All,
@@ -81,27 +84,27 @@ fn convert_to_min(duration: u16) -> String {
 mod tests {
     use crate::view::*;
 
-    #[test]
-    fn flush_work_timer_works_fine() {
-        let mut buf = Vec::<u8>::new();
-        let actual_resp = flush_work_timer(&mut buf, 4, 1);
-        let actual_view = String::from_utf8(buf.to_vec()).unwrap();
+    // #[test]
+    // fn flush_work_timer_works_fine() {
+    //     let mut buf = Vec::<u8>::new();
+    //     let actual_resp = flush_work_timer(&mut buf, 4, 1);
+    //     let actual_view = String::from_utf8(buf.to_vec()).unwrap();
 
-        assert!(actual_resp.is_ok());
-        assert!(actual_view.contains("00:04 (Round 1)"));
-        assert!(actual_view.contains("[Q]: quit, [Space]: pause/resume"));
-    }
+    //     assert!(actual_resp.is_ok());
+    //     assert!(actual_view.contains("00:04 (Round 1)"));
+    //     assert!(actual_view.contains("[Space]: pause/resume"));
+    // }
 
-    #[test]
-    fn flush_break_timer_works_fine() {
-        let mut buf = Vec::<u8>::new();
-        let actual_resp = flush_break_timer(&mut buf, 604, 2);
-        let actual_view = String::from_utf8(buf.to_vec()).unwrap();
+    // #[test]
+    // fn flush_break_timer_works_fine() {
+    //     let mut buf = Vec::<u8>::new();
+    //     let actual_resp = flush_break_timer(&mut buf, 604, 2);
+    //     let actual_view = String::from_utf8(buf.to_vec()).unwrap();
 
-        assert!(actual_resp.is_ok());
-        assert!(actual_view.contains("10:04 (Round 2)"));
-        assert!(actual_view.contains("[Q]: quit, [Space]: pause/resume"));
-    }
+    //     assert!(actual_resp.is_ok());
+    //     assert!(actual_view.contains("10:04 (Round 2)"));
+    //     assert!(actual_view.contains("[Space]: pause/resume"));
+    // }
 
     #[test]
     fn flush_break_interval_works_fine() {
@@ -111,7 +114,7 @@ mod tests {
 
         assert!(actual_resp.is_ok());
         assert!(actual_view.contains("press Enter to take a break"));
-        assert!(actual_view.contains("[Q]: quit, [Enter]: start"));
+        assert!(actual_view.contains("[Enter]: start"));
     }
 
     #[test]
@@ -122,6 +125,6 @@ mod tests {
 
         assert!(actual_resp.is_ok());
         assert!(actual_view.contains("press Enter to work!!"));
-        assert!(actual_view.contains("[Q]: quit, [Enter]: start"));
+        assert!(actual_view.contains("[Enter]: start"));
     }
 }
